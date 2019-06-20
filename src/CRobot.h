f46774893e2ab15e19f0bd2bd924a4e2b62776d9
DongHyun Ahn <root@dhahn>
2019-06-20 12:20:38 +0900

    Trot motion was generated.

--- src/CRobot.h
+++ src/CRobot.h
@@ -6,6 +6,7 @@
 #include <rbdl/addons/urdfreader/urdfreader.h>
 
 #define PI  3.14159265359
+#define PI2  6.28318530718
 #define GRAVITY 9.81
 
 #define onemsec 0.001
@@ -22,6 +23,9 @@
 #define AXIS_Roll  3
 #define AXIS_Pitch 4
 #define AXIS_Yaw   5
+
+#define R2D 57.295779513
+#define D2R 0.0174532925
 
 using namespace std;
 using namespace RigidBodyDynamics;
@@ -112,6 +116,9 @@ typedef struct EndPoint {
     int ID;
 } ENDPOINT;
 
+
+
+
 class CRobot {
    
 public:
@@ -134,10 +141,82 @@ public:
     RigidBodyDynamics::Model* m_pModel; //* URDF Model
     RigidBodyDynamics::Math::VectorNd RobotState;
     RigidBodyDynamics::Math::VectorNd RobotStatedot;
+    RigidBodyDynamics::Math::VectorNd RobotState2dot;
     RigidBodyDynamics::Math::VectorNd BasePosOri;
     RigidBodyDynamics::Math::VectorNd BaseVel;
     RigidBodyDynamics::Math::VectorNd JointAngle;
     RigidBodyDynamics::Math::VectorNd JointVel;
+    
+    MatrixNd M_term = MatrixNd::Zero(19, 19);
+    VectorNd hatNonLinearEffects = VectorNd::Zero(19);
+    VectorNd G_term = VectorNd::Zero(19);
+    VectorNd C_term = VectorNd::Zero(19);
+    VectorNd CTC_Torque = VectorNd::Zero(19);
+    
+    VectorNd EP_RL = Vector3d(0,0,0);
+    VectorNd EP_RR = Vector3d(0,0,0);
+    VectorNd EP_FL = Vector3d(0,0,0);
+    VectorNd EP_FR = Vector3d(0,0,0);
+    
+    double L3_x = 0.025516;
+    double L3_y = 0.0;
+    double L3_z = 0.304515;
+    
+    VectorNd EP_OFFSET_RL = Vector3d( L3_x, L3_y, -L3_z);
+    VectorNd EP_OFFSET_RR = Vector3d( L3_x, L3_y, -L3_z);
+    VectorNd EP_OFFSET_FL = Vector3d(-L3_x, L3_y, -L3_z);
+    VectorNd EP_OFFSET_FR = Vector3d(-L3_x, L3_y, -L3_z);
+    VectorNd Originbase = Vector3d(0, 0, 0);
+    
+    VectorNd actual_EP = VectorNd::Zero(12);
+    VectorNd actual_EP_vel = VectorNd::Zero(12);
+    VectorNd actual_EP_acc = VectorNd::Zero(12);
+    
+    MatrixNd J_RL = MatrixNd::Zero(3,19);
+    MatrixNd J_RR = MatrixNd::Zero(3,19);
+    MatrixNd J_FL = MatrixNd::Zero(3,19);
+    MatrixNd J_FR = MatrixNd::Zero(3,19);
+    MatrixNd J_FRONT_BODY = MatrixNd::Zero(6,19);
+    MatrixNd J_BASE = MatrixNd::Zero(6,19);
+    
+    MatrixNd J_A = MatrixNd::Zero(19,19);
+    
+    VectorNd x_dot = VectorNd::Zero(19);
+    VectorNd ddqZero = VectorNd::Zero(19);
+    
+    VectorNd RL_dJdQ = Vector3d(0,0,0);
+    VectorNd RR_dJdQ = Vector3d(0,0,0);
+    VectorNd FL_dJdQ = Vector3d(0,0,0);
+    VectorNd FR_dJdQ = Vector3d(0,0,0);
+    VectorNd FRONT_BODY_dJdQ = VectorNd::Zero(6);
+    VectorNd base_dJdQ = VectorNd::Zero(6);
+    VectorNd dJdQ = VectorNd::Zero(19);
+    
+    VectorNd Fc = VectorNd::Zero(19);
+    VectorNd x_2dot_cp = VectorNd::Zero(19);
+    
+    VectorNd Kp_EP = VectorNd::Zero(12);//(100,100,100,100,100,100,100,100,100,100,100,100);
+    VectorNd Kd_EP = VectorNd::Zero(12);//(1,1,1,1,1,1,1,1,1,1,1,1);
+    VectorNd target_EP = VectorNd::Zero(12);
+    VectorNd target_EP_vel = VectorNd::Zero(12);
+    VectorNd target_EP_acc = VectorNd::Zero(12);
+    VectorNd goal_EP = VectorNd::Zero(12);//(0,0.218,-0.45,0,-0.218,-0.45,0.7,0.218,-0.45,0.7,-0.218,-0.45);
+    VectorNd init_EP = VectorNd::Zero(12);
+//    VectorNd init_goal_EP = VectorNd::Zero(12);
+    VectorNd trot_goal_EP = VectorNd::Zero(12);
+    
+    
+    
+    
+    double Fc_RL = 0, Fc_RR = 0, Fc_FL = 0, Fc_FR = 0;
+    
+    
+    
+    Quaternion QQ;
+    
+    
+
+    
     
 private:
 };
